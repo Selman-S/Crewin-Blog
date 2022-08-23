3444f628c6df70c305bcb8dd3b717294f1b62a5d
selman <gunduzgece546@gmail.com>
2022-08-23 13:02:24 +0300

    router structure

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,12 +2,20 @@ import React from 'react'
 import { BrowserRouter, Route,
   Routes } from 'react-router-dom'
 import Navbar from '../components/Navbar'
+import Sidebar from '../components/Sidebar'
+import Dashbord from '../pages/Dashbord'
+import Posts from '../pages/Posts'
+import Users from '../pages/Users'
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
       <Navbar />
+      <Sidebar/>
       <Routes>
+        <Route path="/" element={<Dashbord/>}/>
+        <Route path="/users" element={<Users/>}/>
+        <Route path="/posts" element={<Posts/>}/>
      
       </Routes>
     </BrowserRouter>
