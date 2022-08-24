5ea6acac741cd39b6fc1fdbd7776e1555457aae2
selman <gunduzgece546@gmail.com>
2022-08-24 23:45:32 +0300

    news

---
+++ src/components/newscom/NewsCom.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NewsCom = ({newsItem}) => {
+
+  const {title,image,desc} = newsItem
+  return (
+    <div>
+      news
+    </div>
+  )
+}
+
+export default NewsCom
--- src/pages/dashboard/Dashbord.jsx
+++ src/pages/dashboard/Dashbord.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
-
+import NewsCom from '../../components/newscom/NewsCom'
+import {news} from '../../helper/data'
 import './Dashboard.scss'
 const Dashbord = () => {
+  
+
   return (
     <div className="dashboard">
-          
-<p>dashboard</p>
+         <div className="news-container">
+          {news.map((newsItem) =>
+          <NewsCom newsItem={newsItem} key={newsItem.id}/>
+          )}
+          </div> 
+
     </div>
   )
 }
