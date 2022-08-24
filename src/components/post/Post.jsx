b6d80999fa0aa56e2bdf315cf1e6821ea324a93c
selman <gunduzgece546@gmail.com>
2022-08-24 22:37:47 +0300

    posts

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './Post.scss'
+import { BsThreeDotsVertical } from 'react-icons/bs';
 
-const Post = () => {
+
+const Post = ({post}) => {
+  const [toggle, setToggle] = useState(false);
+  const {post_title,desc} = post
   return (
-    <div>
-      post
+    <div className="post">
+      <div className="three-point">
+        <BsThreeDotsVertical />
+        <div className="edit" style={{display:toggle ? 'block':'none'}}>
+          <div className="edit-links">
+
+          <p className="edit-link">Edit</p>
+          <p className="delete-link">Delete</p>
+          </div>
+        </div>
+      </div>
+      <h3 className="post-title">
+        {post_title}
+      </h3>
+      <p className="post-desc">
+    {desc}
+      </p>
     </div>
   )
 }
--- src/pages/posts/Posts.jsx
+++ src/pages/posts/Posts.jsx
@@ -4,13 +4,16 @@ import './Posts.scss'
 import {posts} from '../../helper/data'
 import Post from '../../components/post/Post'
 const Posts = () => {
-  console.log(posts);
+  
   return (
     <div className="posts">
 <h1 className="title">Posts</h1>
+<div className="post-container">
+
      {posts.map((post) =>
      <Post post={post}  key={post.id}/>
      )}
+     </div>
     </div>
   )
 }
